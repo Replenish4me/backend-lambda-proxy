5dffe43cadbbf726120e6ef51fb48efbc4cd906d
CaioRuizz <caio.ruiz00@gmail.com>
2023-04-05 20:40:12 -0300

    tentando corrigir role ja existe

--- infra/main.tf
+++ infra/main.tf
@@ -13,7 +13,20 @@ resource "aws_lambda_function" "my_function" {
 }
 
 resource "aws_iam_role" "my_role" {
-  name = "my-lambda-role"
+  name = "lambda-proxy-role-${var.env}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "my_policy_attachment" {

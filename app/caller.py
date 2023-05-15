10f5d664ec30291d814da952f4b6adcc5b63514f
CaioRuizz <caio.ruiz00@gmail.com>
2023-05-15 02:40:00 -0300

    código lambda proxy

--- app/caller.py
+++ app/caller.py
@@ -6,9 +6,10 @@ import boto3
 def call_lambda(name: str, payload: Dict[str, Any]) -> Dict[str, Any]:
     """Call a lambda function and return its response."""
     client = boto3.client('lambda')
+    print(payload)
     response = client.invoke(
         FunctionName=name,
         InvocationType='RequestResponse',
-        Payload=json.dumps(payload).encode('utf-8'),
+        Payload=json.dumps(payload),
     )
     return json.loads(response['Payload'].read().decode('utf-8'))
--- app/handler.py
+++ app/handler.py
@@ -9,27 +9,56 @@ def lambda_handler(event: Dict[str, Dict[str, Any]], context: Dict[str, Any]):
     req_headers = event.get('headers') or {}
     path = event.get('path') or '/'
 
-    function_name : str = routes.get(path, {}).get('function_name')
-    requires_auth : bool = routes.get(path, {}).get('requires_auth')
+    function_name : str = routes.get(path, {})['function_name']
+    requires_auth : bool = routes.get(path, {})['requires_auth']
+    required_params : list = routes.get(path, {})['required_params']
+
+    # Verifica se todos os parâmetros obrigatórios estão presentes
+    for param in required_params:
+        if req_body.get(param) is None:
+            return {
+                'statusCode': 400,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json',
+                },
+                'body': {
+                    'message': f'Parameter {param} is required',
+                },
+            }
 
     if requires_auth:
         response = call_lambda(
             f'validate-session-{os.environ.get("env", "dev")}',
             {'token': req_headers.get('Authorization', '')}
         )
+
+        if response.get('headers') is None:
+            response['headers'] = {}
+        
+        response['headers']['Access-Control-Allow-Origin'] = '*'
+        response['headers']['Content-Type'] = 'application/json'
+
         if response.get('statusCode') != 200:
             return response
-    
-    print(f'Event: {event}')
-    print(f'Request body: {req_body}')
-    print(f'Query params: {query_params}')
-    print(f'Request headers: {req_headers}')
 
     # Invoca a função lambda com os dados em um json não aninhado
     response = call_lambda(
         function_name,
         {**req_body, **query_params, **req_headers}
     )
+
+    if 'statusCode' not in response:
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+            },
+            'body': {
+                'message': 'Internal server error',
+            },
+        }
 
     # Acidiona os headers de resposta caso não existam
     if response.get('headers') is None:

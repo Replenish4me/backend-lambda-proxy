1e0df5d51c3170b0f7a096be475ffff47661becd
CaioRuizz <caio.ruiz00@gmail.com>
2023-05-15 02:19:03 -0300

    fixed main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -10,16 +10,10 @@ resource "aws_lambda_function" "my_function" {
   lifecycle {
     create_before_destroy = true
   }
-
-  environment {
-    variables = {
-      env = var.env
-    }
-  }
 }
 
 resource "aws_iam_role" "my_role" {
-  name = "lambda-proxy-role-${var.env}"
+  name = "replenish4me-${var.function_name}-role-${var.env}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -38,4 +32,50 @@ resource "aws_iam_role" "my_role" {
 resource "aws_iam_role_policy_attachment" "my_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.my_role.name
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_policy" "secrets_manager_policy" {
+  name        = "${var.function_name}-secrets-manager-policy-${var.env}"
+  description = "Policy to allow access to Secrets Manager"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ],
+        Resource = "arn:aws:secretsmanager:${var.region}:${data.aws_caller_identity.current.account_id}:secret:*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "secrets_manager_policy_attachment" {
+  policy_arn = aws_iam_policy.secrets_manager_policy.arn
+  role       = aws_iam_role.my_role.id
+}
+
+resource "aws_iam_policy" "describe_database_policy" {
+  name        = "${var.function_name}-describe-database-policy-${var.env}"
+  description = "Policy to allow access to Describe Database"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "rds:Describe*"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "describe_database_policy_attachment" {
+  policy_arn = aws_iam_policy.describe_database_policy.arn
+  role       = aws_iam_role.my_role.id
 }

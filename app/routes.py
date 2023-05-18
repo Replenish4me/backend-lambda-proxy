95f9fa2596d826f63cb837ef4e6aa99b5b5b3899
CaioRuizz <caio.ruiz00@gmail.com>
2023-05-18 20:56:26 -0300

    adicionado version via query parameter

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,20 @@ import os
 
 routes = {
     '/signup': {
-        'function_name': f'create-user-{os.environ.get("env", "dev")}',
+        'versions': {
+            'v1': {
+                'function_name': f'create-user-{os.environ.get("env", "dev")}',
+            }
+        },
         'requires_auth': False,
         'required_params': ['email', 'senha', 'endereco', 'nome', 'telefone'],
     },
     '/login': {
-        'function_name': f'login-{os.environ.get("env", "dev")}',
+        'version': {
+            'v1': {
+                'function_name': f'login-{os.environ.get("env", "dev")}',
+            }
+        },
         'requires_auth': False,
         'required_params': ['email', 'senha'],
     },
